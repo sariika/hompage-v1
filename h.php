6e1a3a7ed99307f2a8c6bf048356f8f365a94204
sariika <b00164486@mytudublin.ie>
2025-02-19 01:12:23 +0000

    homepage

---
+++ h.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<header><h1 id="title">Up-to-the-minute</h1>
+    <nav>
+       <ul>
+           <li class="nav-item">Home</li>
+           <li class="nav-item">Search page</li>
+           <li class="nav-item">product</li>
+        
+        
+        </ul>
+    </nav>
+    </header>
+    
+<body>
+    <div class="container">
+        <div class="show" >1</div>
+        <div class="show" >2</div>
+        <div class="show" >3</div>
+        <div class="show" >4</div>
+    
+    </div>
+    
+    
+</body>
+<?php
+    for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
+?>
+</html>
